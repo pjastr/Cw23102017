91ee4015f95642288c73bcb96ab2251d15506f80
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-23 13:02:37 +0200

    Add files via upload

---
+++ ConsoleApplication26/ConsoleApplication26/Samochod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Samochod
+    {
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private double cena;
+
+        public double Cena
+        {
+            get { return cena; }
+            set { cena = value; }
+        }
+
+        public string Marka { get; set; }
+        
+
+        //public void UstawMarke(string marka)
+        //{
+        //    this.marka = marka;
+        //}
+
+        //public string PobierzMarke()
+        //{
+        //    return marka;
+        //}
+    }
+}
